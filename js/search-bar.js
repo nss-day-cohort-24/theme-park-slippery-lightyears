1ed372b78538230e91afc5646981e6696136eab0
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-23 14:55:26 -0600

    Added alert when the enter key is pressed but the input is empty

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,18 +1,23 @@
 "use strict";
 
+// Initializes an empty object to contain the function that will be exported to "main.js"
 let search = {};
 
+// Arrow function gets input from user on keyup for the "Enter" key 
 search.captureInput = () => {
 
-// Grabs the search bar element
+// Grabs the search bar element from index.html
 const searchBar = document.getElementById("nav-search-bar");
-console.log("What is searchBar?", searchBar);
 
 // Gets the content of the input field on keyup of the "Enter" key
 searchBar.addEventListener("keyup", function(e) {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 && e.target.value != "") {
+// Stores the value from the input area into a new variable on "Enter" as long as it is not empty
     let userInput = e.target.value.toLowerCase();
     console.log(userInput);
+// If the input is empty and the enter key is pressed, an alert will display asking for input from the user
+    } else if (e.keyCode === 13) {
+        window.alert("Please enter something to search for.");
     }
 });
 };
