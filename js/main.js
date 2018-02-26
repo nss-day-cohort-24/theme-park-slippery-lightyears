193c59d5e2f50859017e704d9b6556a28f8627bb
Jesie Oldenburg <kitglo46@gmail.com>
2018-02-25 21:12:59 -0600

    added functionality for list items

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ let fetchObjects = require("./fetch");
 let searchObjects = require("./search-bar");
 let domObjects = require("./domOnClicks");
 let mapAreas = require("./map");
-
+let attractionDetails = require("./display-area");
 
 searchObjects.search.captureInput();
 domObjects.getMapSearchResults();
 domObjects.getTimeSearchResults();
 
 mapAreas.mapAreaData();
+attractionDetails.buildShit();
